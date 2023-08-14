182b6cab8e79bb59ea4b6d9e3c7ff4aa5c1a783e
jeonghyun <ptr1129@paycrux.co.kr>
2023-08-14 10:51:48 +0900

    Update : remove notes

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,17 @@ const webpack = require("webpack");
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-  mode: prod
-    ? "production"
-    : "development" /* webpack serve시 inline에서 mode 플래그로 node_env 제어 가능 */,
-  devtool: prod
-    ? "hidden-source-map"
-    : "eval" /* 소스맵 노출 여부. hidden-source-map을 통해 소스까기 방지 */,
+  mode: prod ? "production" : "development",
+  devtool: prod ? "hidden-source-map" : "eval",
   devServer: {
-    historyApiFallback: true /* API 404 code 발생시 최초 라우팅 지점으로 자동 이동 */,
-    port: 3000 /* 개발서버 포트 지정 */,
-    hot: true /* 핫리로드 기능 */,
-    open: true /* 개발 구동시 웹페이지 자동 열림 설정 */,
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    open: true,
   },
-  entry: "./src/index.tsx" /* 최초 진입점 경로 지정 */,
+  entry: "./src/index.tsx",
   resolve: {
-    extensions: [
-      ".js",
-      ".jsx",
-      ".ts",
-      ".tsx",
-    ] /* import 경로 지정시 익스텐션 작성 생략 */,
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   module: {
     rules: [
@@ -35,16 +26,16 @@ module.exports = {
     ],
   },
   output: {
-    path: path.join(__dirname, "/dist") /* 빌드 출력물 경로 지정 */,
-    filename: "bundle.js" /* 빌드 출력물 이름 지정 */,
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
   },
   plugins: [
     new webpack.ProvidePlugin({
       React: "react",
-    }) /* import React 생략. 자주 사용되는 모듈들을 미리 등록하여 매번 작성할 필요를 덜음. */,
-    new webpack.HotModuleReplacementPlugin() /* cmd / ctrl + s 시 자동 리렌더링*/,
+    }),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-    }) /* SPA 렌더링 지점을 public/index.html로 지정 */,
+    }),
   ],
 };
